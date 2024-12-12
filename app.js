36e7b7a4668f63dce22b16af27b767aabda857b3
Minseok Kim^C <ksj5147@gmail.com>
2024-12-12 01:21:23 -0800

    fix

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-console.log(age, parseInt(age))
+const age = parseInt(prompt('How old are you?'))
+const krAge = parseInt(age) + 2
+
+console.log("I'm " + age + ' yrs old, but ' + krAge + ' yrs old by Korean age')
