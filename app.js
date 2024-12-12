e524dadee0a232b9ce88d21572d1a58765da0bf7
Minseok Kim^C <ksj5147@gmail.com>
2024-12-12 15:01:22 -0800

    console.log test

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 const age = parseInt(prompt('How old are you?'))
 const krAge = parseInt(age) + 2
 
-console.log("I'm " + age + ' yrs old, but ' + krAge + ' yrs old by Korean age')
+// console.log(isNaN(age))
+
+if (isNaN(age)) {
+  console.log('Please write your age by number')
+} else if (age < 18) {
+  console.log('You are under age')
+} else if (age >= 18 && age <= 50) {
+  console.log('you can drink')
+} else {
+  console.log('stop to drink')
+}
